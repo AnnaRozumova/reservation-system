49b5ed65a7298e456cc8df3052a66f5ba90cec0e
AnnaRoz <anna.rozumova108@gmail.com>
2024-06-11 16:15:39 +0200

    feat: polls app specifications

--- reservationsystem/polls/admin.py
+++ reservationsystem/polls/admin.py
@@ -1,7 +1,25 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Survey, Question, Choice, SurveyQuestion
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class SurveyQuestionInline(admin.TabularInline):
+    model = SurveyQuestion
+    extra = 1
+
+class SurveyAdmin(admin.ModelAdmin):
+    inlines = [SurveyQuestionInline]
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active',)
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(SurveyQuestion)
