8fcd2cacafb4ff59aeefeeb54b3e2b25b8450f6b
AnnaRoz <anna.rozumova108@gmail.com>
2024-06-13 13:23:10 +0200

    Initial commit, creating reservation-system project from scratch version 1.1

--- reservationsystem/survey_responses/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'survey_responses'
-urlpatterns = [
-    path('survey/<int:survey_id>/', views.survey_response_view, name='survey_response'),
-    path('thank_you/', views.thank_you_view, name='thank_you'),
-]
